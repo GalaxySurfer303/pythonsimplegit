0866fcede30b46f2c9160a0d4bc7d500c5679b91
GalaxySurfer303 <oldsamo1980@gmail.com>
2024-09-23 19:38:03 +0200

    Add files via upload

---
+++ pythonProject1/main.py
@@ -0,0 +1,52 @@
+for i in range(3):
+    print('love ' + 'is ' + 'in ' + 'the ' + 'air ')
+
+
+    print('bububu' * 10)
+    print('\n\n\n\n')
+
+else:
+    print('\n\n\n\n')
+    print('sprawdzam czy tu:  \npojawi się: \nE\nN\nT\nE\nR')
+    print(100 * 10 * 10 ** 10 + (0-100000000000)+ 1000000)
+
+    wynik = (1 * 2 / 3 * 4 / 5 * 6 / 7 * 8 / 9 * 10)
+    if  wynik > 1:
+        print('czyli jest wiecej niz 1')
+        print('\n\n\n\n')
+    else:
+        print(wynik)
+        print('\n\n\n\n')
+
+
+num_1 = 100
+num = 10
+print('print(num)=10')
+print(num)
+
+num = 10
+
+
+
+num = num + 1
+
+print('num=num+1')
+print(num)
+
+num += num
+print(num)
+
+num += num
+print('num=num+1 == num+=num')
+print(num)
+
+num_1+=num
+print('num_1=num_1+num == num_1+=num')
+print(num_1)
+print('\n\n\n\n')
+
+print('num = ' + num )
+print('num_1 = ' + num_1 )
+
+
+
